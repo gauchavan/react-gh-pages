ad76e3d1d7f904420d9241d7a24c155027ab4934
Gauri Chavan Office <gaurichavanoffice@gauris-MacBook-Pro.local>
2020-08-15 18:18:32 -0400

    Updates

---
+++ precache-manifest.cd7cd2e944790e2274401a543651af58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b6144eb785afb330d349fb5806b2b8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "de491cb977e1b44d7f8c",
+    "url": "/react-gh-pages/static/css/main.cca07ea3.chunk.css"
+  },
+  {
+    "revision": "ddc31dda192a185deb02",
+    "url": "/react-gh-pages/static/js/2.1868b251.chunk.js"
+  },
+  {
+    "revision": "de491cb977e1b44d7f8c",
+    "url": "/react-gh-pages/static/js/main.6b677202.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "0d2c0d4f54b4497350d49eef90e56253",
+    "url": "/react-gh-pages/static/media/background.0d2c0d4f.jpg"
+  }
+]);
